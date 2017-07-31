7ce4ac68361d2f5f993dff46122407d8660a1df5
sown5ot <sonnguyenhoang2310@gmail.com>
2017-08-01 00:42:16 +0700

    Add player

--- src/touhou/GameWindow.java
+++ src/touhou/GameWindow.java
@@ -1,8 +1,13 @@
 package touhou;
 
+import tklibs.SpriteUtils;
+
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by huynq on 7/29/17.
@@ -11,8 +16,18 @@ public class GameWindow extends Frame {
 
     private long lastTimeUpdate;
     private long currentTime;
+    private Graphics2D windowGraphics;
+
+    private BufferedImage backbufferImage;
+    private Graphics2D backbufferGraphics;
+
+
+    private BufferedImage background;
+    private BufferedImage player;
 
     public GameWindow() {
+        background = SpriteUtils.loadImage("assets/images/background/0.png");
+        player = SpriteUtils.loadImage("assets/images/players/straight/3.png");
         setupGameLoop();
         setupWindow();
     }
@@ -25,10 +40,31 @@ public class GameWindow extends Frame {
         this.setSize(1024, 768);
         this.setTitle("Tohou - Remade by Sown5ot");
         this.setVisible(true);
+
+        this.backbufferImage = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+        this.backbufferGraphics = (Graphics2D) this.backbufferImage.getGraphics();
+        this.windowGraphics = (Graphics2D) this.getGraphics();
+
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
+            }
+        });
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
             }
         });
     }
@@ -51,6 +87,11 @@ public class GameWindow extends Frame {
     }
 
     private void render() {
+        backbufferGraphics.setColor(Color.black);
+        backbufferGraphics.fillRect(0,0,1024,768);
+        backbufferGraphics.drawImage(background, 0, 0, null);
+        backbufferGraphics.drawImage(player, 100, 100, null);
 
+        windowGraphics.drawImage(backbufferImage, 0, 0, null);
     }
 }
